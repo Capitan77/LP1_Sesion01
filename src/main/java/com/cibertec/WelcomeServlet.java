d59b12144e86bad45673a99b9b94f0fc24a3bf37
Capitan77 <dael202106@gmail.com>
2024-11-17 17:01:23 -0500

    Ejercicios JSP

---
+++ src/main/java/com/cibertec/WelcomeServlet.java
@@ -0,0 +1,29 @@
+package com.cibertec;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+//otra manera de generar la relacion entre un servlet al usuario
+@WebServlet("/Welcome")
+//se vuelve un servlet gracias a la extensión
+public class WelcomeServlet extends HttpServlet {
+
+
+    //Llamamos al metodo do "Get" por medio de " ALT + INS " para modificarlo
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       // super.doGet(req, resp);
+
+        //cada vez que un usuario ingrese al "HellServlet" te lanzará un saluda HTML
+           resp.setContentType("text/html");
+           resp.getWriter().println("<h1>Hola desde WelcomeServlet</h1>");
+           resp.getWriter().println("<p>Este es un mensaje guiado dinámicamente desde WelcomeServlet</p>");
+
+    }
+}
