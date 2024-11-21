c3f756fa8f2844de1d6bc4a7db2b39810bb75941
Capitan77 <dael202106@gmail.com>
2024-11-21 18:05:37 -0500

    Nada

--- src/main/java/com/cibertec/SaludoServlet.java
+++ src/main/java/com/cibertec/SaludoServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-//Coneccion con el usuario
+//Coneccion con el usuario -- garcias a JAKARTA en POM.XML
 @WebServlet("/SaludoServlet")
 
 public class SaludoServlet extends HttpServlet {
--- target/classes/ejercicio04.jsp
+++ target/classes/ejercicio04.jsp
@@ -23,6 +23,5 @@
     </form>
 
 
-
 </body>
 </html>
